d99966ee6a2f853cc96051920da24a8371566c12
Neha Singh <97676251+26neha@users.noreply.github.com>
2022-02-28 07:46:55 +0530

    Fault tolerance file added.

---
+++ Micro/hys-dashboard/src/test/java/com/dashboard/hysdashboard/HysDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.hysdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HysDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
